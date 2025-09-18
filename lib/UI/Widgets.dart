fad14985682d61e8b90ca36d4a82252bf6c16e9c
Munim57 <munim1857@gmail.com>
2025-09-18 16:59:27 +0500

    new

--- lib/UI/Widgets.dart
+++ lib/UI/Widgets.dart
@@ -9,85 +9,199 @@ class ContainerApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blue,
-          title: Center(child: Text('ContainerApp')),
+          title: Center(child: Text('Top Widgets Explanations')),
         ),
         body: Column(
           // column use for vertical alignment and row use for horizantel alignment
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    color: Colors.red,
-                    height: 200,
-
-                    child: Center(child: Text('Container 1')),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    height: 200,
-                    color: Colors.green,
-                    child: Center(child: Text('Container 2')),
-                  ),
-                ),
-              ],
-            ),
-            Center(
-              // use for center the container
-              child:
-                  //use for create container and use for design and style and box decoration and many more
-                  //and also use for size height and width and its expalnation below about container properties
-                  Container(
-                    height: 200,
-                    width: 150,
-
-                    // transform: Matrix4.rotationZ(.2),          // use for rotate
-                    // margin: EdgeInsets.only(right: 0, left: 0),      // use for outside space
-                    //padding: EdgeInsets.all(30),     // use for inside space for any text or widget
-                    decoration: BoxDecoration(
-                      color: Colors.orange,
-                      borderRadius: BorderRadius.circular(
-                        20,
-                      ), // use for container border radius
-                      // borderRadius: BorderRadius.only(
-                      //   topRight: Radius.circular(50),
-
-                      //   bottomLeft: Radius.circular(50),
-
-                      //   topLeft: Radius.circular(10),
-
-                      //   bottomRight: Radius.circular(10),
-                      // ),
-                      // border: Border.all(color: Colors.blue, width: 20),
-                      image: DecorationImage(
-                        fit: BoxFit
-                            .fill, // use for image fit in container and use for image cover full container
-
-                        image: NetworkImage(
-                          'https://images.pexels.com/photos/15920486/pexels-photo-15920486.jpeg',
-                        ),
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.blueGrey,
-                          blurRadius: 10,
-                        ), // use for shadow effect for container
-                      ],
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Login 1',
-                        style: TextStyle(color: Colors.white, fontSize: 30),
-                      ),
-                    ),
-                  ),
-            ),
-          ],
+          children: [RichText(text: TextSpan())],
         ),
       ),
     );
   }
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+// Circle Avatar widget 
+
+  //  Center(
+  //             child: CircleAvatar(
+  //               radius: 100,
+  //               // maxRadius: 100,
+  //               // minRadius: 50,
+  //               backgroundColor: Colors.black,
+  //               // child: Icon(Icons.person),
+  //               backgroundImage: NetworkImage(
+  //                 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg',
+  //               ),
+  //             ),
+  //           ),
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// Divider widget
+
+//  Divider(
+//               color: Colors.black,
+//               thickness: 0.5,
+//             ), // use for divider the line
+//             SizedBox(
+//               height: 50,
+//               child: VerticalDivider(color: Colors.black, thickness: 0.5),
+//             ),
+//             Divider(color: Colors.black, thickness: 0.5), 
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+//Stack widget and positined widget and sizedbox widget and align widget 
+
+//  Center(
+//               child: SizedBox(
+//                 child: Stack(
+//                   // stack use for overlapping the container and widgets
+//                   alignment: Alignment
+//                       .center, // use for alignment the text in container
+//                   children: [
+//                     Container(
+//                       height: 140,
+//                       width: 140,
+//                       color: Colors.red,
+//                       child: Center(child: Text('Container 1')),
+//                     ),
+//                     Container(
+//                       height: 120,
+//                       width: 120,
+//                       color: Colors.yellow,
+//                       child: Center(child: Text('Container 2')),
+//                     ),
+//                     Container(
+//                       height: 100,
+//                       width: 100,
+//                       color: Colors.green,
+//                       child: Center(child: Text('Container 3')),
+//                     ),
+//                     Container(height: 80, width: 80, color: Colors.blue),
+//                     Positioned(
+//                       // top: 10,
+//                       // bottom: 10,
+//                       // right: 10,
+//                       child: Text('Container'),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             ),
+
+
+///////////////////////////////////////////////////////////////////////////////////
+//Expended widget
+
+// Expanded(
+//               flex: 9,
+//               child: Container(
+//                 height: 300,
+//                 color: Colors.red,
+//                 child: Center(child: Text('Container')),
+//               ),
+//             ),
+
+////////////////
+/// expended widget use for expanded the conatiner according to screen size
+//  Expanded(
+//                   // use for expand the container according to screen size
+//                   flex:
+//                       5, //  used for space according to flex value and its optional
+//                   child: Container(
+//                     color: Colors.red,
+
+//                     height: 200,
+//                     child: Center(child: Text('Container 1')),
+//                   ),
+//                 ),
+//                 Expanded(
+//                   flex: 3,
+//                   child: Container(
+//                     height: 200,
+//                     color: Colors.green,
+//                     child: Center(child: Text('Container 2')),
+//                   ),
+//                 ),
+//                 Expanded(
+//                   flex: 2,
+
+//                   child: Container(
+//                     height: 200,
+//                     color: Colors.blue,
+//                     child: Center(child: Text('Container 3')),
+//                   ),
+//                 ),
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////
+///cotainer properties
+  // Center(
+  //             // use for center the container
+  //             child:
+  //                 //use for create container and use for design and style and box decoration and many more
+  //                 //and also use for size height and width and its expalnation below about container properties
+  //                 Container(
+  //                   height: 200,
+  //                   width: 150,
+
+  //                   // transform: Matrix4.rotationZ(.2),          // use for rotate
+  //                   // margin: EdgeInsets.only(right: 0, left: 0),      // use for outside space
+  //                   //padding: EdgeInsets.all(30),     // use for inside space for any text or widget
+  //                   decoration: BoxDecoration(
+  //                     color: Colors.orange,
+  //                     borderRadius: BorderRadius.circular(
+  //                       20,
+  //                     ), // use for container border radius
+  //                     // borderRadius: BorderRadius.only(
+  //                     //   topRight: Radius.circular(50),
+
+  //                     //   bottomLeft: Radius.circular(50),
+
+  //                     //   topLeft: Radius.circular(10),
+
+  //                     //   bottomRight: Radius.circular(10),
+  //                     // ),
+  //                     // border: Border.all(color: Colors.blue, width: 20),
+  //                     image: DecorationImage(
+  //                       fit: BoxFit
+  //                           .fill, // use for image fit in container and use for image cover full container
+
+  //                       image: NetworkImage(
+  //                         'https://images.pexels.com/photos/15920486/pexels-photo-15920486.jpeg',
+  //                       ),
+  //                     ),
+  //                     boxShadow: [
+  //                       BoxShadow(
+  //                         color: Colors.blueGrey,
+  //                         blurRadius: 10,
+  //                       ), // use for shadow effect for container
+  //                     ],
+  //                   ),
+  //                   child: Center(
+  //                     child: Text(
+  //                       'Login 1',
+  //                       style: TextStyle(color: Colors.white, fontSize: 30),
+  //                     ),
+  //                   ),
+  //                 ),
+  //           ),
