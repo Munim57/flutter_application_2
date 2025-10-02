b33acff5e43c62bc6a8ab332bc73ea9c39a73854
Munim57 <munim1857@gmail.com>
2025-10-02 12:37:54 +0500

    mediaquery

---
+++ lib/media_query/media_query.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Mediaquery());
+}
+
+class Mediaquery extends StatefulWidget {
+  const Mediaquery({super.key});
+
+  @override
+  State<Mediaquery> createState() => _MediaqueryState();
+}
+
+class _MediaqueryState extends State<Mediaquery> {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height - kTextTabBarHeight;
+    double width = MediaQuery.of(context).size.width;
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Center(child: Text('Media Query')),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Text('Height ${height.toString()}'),
+              Text('Width ${width.toString()}'),
+            ],
+          ),
+        ),
+      ),
+    );
+    ;
+  }
+}
